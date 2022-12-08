eeb69938c021330819298716a4b6bc84d5fcc07f
Loki99911 <nikita_kresik@i.ua>
2022-12-08 08:52:41 +0200

    7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,8 +16,8 @@ export class App extends Component {
   };
 
   addNewContact = ({ name, number }) => {
-    if (this.state.contacts.find(contact => contact.name)) {
-      return alert('ASSS');
+    if (this.state.contacts.find(contact => contact.name===name)) {
+      return alert(`${name} is already in contacts!`);
     }
 
     const contact = { id: nanoid(), name, number };
